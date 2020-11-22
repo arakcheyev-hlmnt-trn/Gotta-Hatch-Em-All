6923f50f8c1a9fe308d3e0e2859bf7bcd657e038
aarakcheyev <arakcheyev.as@haulmont.com>
2020-11-22 17:51:17 +0400

    Step 2. Display a List

--- angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
 
 @Component({
   selector: 'app-heroes',
@@ -8,14 +9,21 @@ import { Hero } from '../hero';
 })
 export class HeroesComponent implements OnInit {
 
+  heroes: Hero[] = HEROES;
+
   hero: Hero = {
     id: 1,
     name: 'Windstorm'
   };
+
+  selectedHero!: Hero;
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  onSelect(hero: Hero) {
+    this.selectedHero = hero;
+  }
 }
