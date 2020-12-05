a6568417b12495e1cb6a59e5629148b2c9872ed8
aarakcheyev <arakcheyev.as@haulmont.com>
2020-12-05 21:01:58 +0400

    Vue Lesson 2: Creating the Vue Application

---
+++ vue_store/main.js
@@ -0,0 +1,10 @@
+const app = Vue.createApp({
+    data() {
+        return {
+            product: 'Socks',
+            // Solution
+            description: 'A warm fuzzy pair of socks.' 
+            // Solution
+        }
+    }
+})
