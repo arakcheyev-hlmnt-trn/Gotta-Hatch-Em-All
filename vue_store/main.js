98ad3c6956d1e88fdf1ca750fbc7ff924abc9061
aarakcheyev <arakcheyev.as@haulmont.com>
2020-12-05 21:39:14 +0400

    Vue Lesson 3: Attribute Binding

--- vue_store/main.js
+++ vue_store/main.js
@@ -2,9 +2,8 @@ const app = Vue.createApp({
     data() {
         return {
             product: 'Socks',
-            // Solution
-            description: 'A warm fuzzy pair of socks.' 
-            // Solution
+            image: './assets/images/socks_green.jpg',
+            link: 'https://www.google.com',
         }
     }
 })
