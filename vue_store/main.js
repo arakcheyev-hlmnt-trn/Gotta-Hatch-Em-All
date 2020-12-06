bdf5490a32aa9e854f8c7a8abb0c46fadf4bc8fe
aarakcheyev <arakcheyev.as@haulmont.com>
2020-12-06 12:56:25 +0400

    Vue Lesson 5: List Rendering (finish code)

--- vue_store/main.js
+++ vue_store/main.js
@@ -4,7 +4,11 @@ const app = Vue.createApp({
             product: 'Socks',
             image: './assets/images/socks_blue.jpg',
             inStock: true,
-            details: ['50% cotton', '30% wool', '20% polyester']
+            details: ['50% cotton', '30% wool', '20% polyester'],
+            variants: [
+                {id: 2234, color: 'green'},
+                {id: 2235, color: 'blue'}
+            ]
         }
     }
 })
